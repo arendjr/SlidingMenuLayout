5060627d1a4bb78584b1bc47785ddd93df24b365
Arend van Beelen jr <arendjr@gmail.com>
2012-09-27 10:42:12 +0200

    Add SlidingMenuLayout.java.

---
+++ SlidingMenuLayout.java
@@ -0,0 +1,139 @@
+package nl.arendjr.slidingmenulayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SlidingMenuLayout extends ViewGroup {
+
+    public SlidingMenuLayout(Context context) {
+
+        super(context);
+
+        init();
+    }
+
+    public SlidingMenuLayout(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+
+        init();
+    }
+
+    public SlidingMenuLayout(Context context, AttributeSet attrs, int defStyle) {
+
+        super(context, attrs, defStyle);
+
+        init();
+    }
+
+    public boolean isOpen() {
+
+        return m_open;
+    }
+
+    public void openMenu() {
+
+        if (m_open) {
+            return;
+        }
+
+        m_currentDX = 0;
+        m_timer = new Timer();
+        m_timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                m_currentDX += m_slidedDX / 10;
+                if (m_currentDX >= m_slidedDX) {
+                    m_currentDX = m_slidedDX;
+                    m_timer.cancel();
+                }
+                ((Activity) getContext()).runOnUiThread(new Runnable() {
+                    public void run() {
+                        requestLayout();
+                    }
+                });
+            }
+        }, 16, 16);
+
+        m_open = true;
+    }
+
+    public void closeMenu() {
+
+        if (!m_open) {
+            return;
+        }
+
+        m_timer = new Timer();
+        m_timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                m_currentDX -= m_slidedDX / 10;
+                if (m_currentDX <= 0) {
+                    m_currentDX = 0;
+                    m_timer.cancel();
+                }
+                ((Activity) getContext()).runOnUiThread(new Runnable() {
+                    public void run() {
+                        requestLayout();
+                    }
+                });
+            }
+        }, 16, 16);
+
+        m_open = false;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        super.setMeasuredDimension(width, height);
+
+        final int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            View view = getChildAt(i);
+            measureChild(view, MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY), heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+
+        int width = right - left;
+        int height = bottom - top;
+
+        m_slidedDX = width * 4 / 5;
+
+        final int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            View view = getChildAt(i);
+            if (i == count - 1) {
+                view.layout(m_currentDX, 0, width + m_currentDX, height);
+            } else {
+                view.layout(0, 0, m_slidedDX, height);
+            }
+        }
+    }
+
+    private void init() {
+
+        setChildrenDrawingOrderEnabled(true);
+    }
+
+    private boolean m_open = false;
+
+    private int m_currentDX = 0;
+    private int m_slidedDX = 0;
+
+    private Timer m_timer;
+}
